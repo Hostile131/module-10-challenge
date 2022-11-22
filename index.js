3746f0e69258d3bf618eaa2a6dcb915dd71b42d8
Ben Fifield <actuallysmart@yahoo.com>
2022-11-22 13:10:55 -0800

    create html code and questions

--- index.js
+++ index.js
@@ -1,88 +1,118 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const jest = require('jest');
 
-const generateREADME = ({ appName, licenseBadge, descriptionOne, descriptionTwo, descriptionThree, descriptionFour, installation, usage, license, licenseURL, contributing, test, githubUsername, emailAddress }) =>
-`# ${appName}
-
-${licenseBadge}
-
-## Description
-
-- Motivation for this project: ${descriptionOne}
-- Reason for this project: ${descriptionTwo}
-- Problem(s) solved by this project: ${descriptionThree}
-- Lessons learned: ${descriptionFour}
-
-## Table of Contents
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Installation
-
-${installation}
-
-## Usage
-
-${usage}
-
-## License
-
-[${license}](${licenseURL})
-
-## Contributing
-
-${contributing}
-
-## Tests
-
-${test}
-
-## Questions
-
-- [GitHub profile](https://www.github.com/${githubUsername})
-- [${emailAddress}](mailto:${emailAddress})`
+// generate HTML
+const generateHTML = ({ 
+    // all the form filled blocks
+    managerName, licenseBadge, descriptionOne, descriptionTwo, descriptionThree, descriptionFour, installation, usage, license, licenseURL, contributing, test, githubUsername, emailAddress }) =>
+`
+// This is where the HTML skeleton goes
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>${managerName}</p>
+</body>
+</html>
+`
 
 inquirer
     .prompt([
         {
             type: 'input',
-            name: 'appName',
-            message: 'What is the name of your application?',
+            name: 'managerName',
+            message: 'What is the name of your team manager?',
         },
         {
             type: 'input',
-            name: 'descriptionOne',
-            message: 'Briefly describe your application. What was your motivation for making this application?',
+            name: 'managerID',
+            message: "What is your team manager's employee ID?",
         },
         {
             type: 'input',
-            name: 'descriptionTwo',
-            message: 'Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")'
+            name: 'managerEmail',
+            message: "What is your team manager's email address?",
         },
         {
             type: 'input',
-            name: 'descriptionThree',
-            message: 'What problem does this application solve?'
+            name: 'managerOfficeNumber',
+            message: "What is your team manager's office number?",
+        },
+        {
+            type: 'list',
+            name: 'addEngineerOrInternOne',
+            message: 'Would you like to add an Engineer or an Intern next?',
+            choices: ['Engineer', 'Intern']
         },
         {
             type: 'input',
-            name: 'descriptionFour',
-            message: 'What did you learn?'
+            name: 'internName',
+            message: 'What is the name of this intern?',
+            when(answers) {
+                return answers.addEngineerOrInternOne === 'Intern'
+            }
         },
         {
             type: 'input',
-            name: 'installation',
-            message: 'How do you install your application?',
+            name: 'internID',
+            message: "What is this intern's employee ID?",
+            when(answers) {
+                return answers.addEngineerOrInternOne === 'Intern'
+            }
         },
         {
             type: 'input',
-            name: 'usage',
-            message: 'How is your application used?',
+            name: 'internEmail',
+            message: "What is this intern's email address?",
+            when(answers) {
+                return answers.addEngineerOrInternOne === 'Intern'
+            }
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "What school does this intern go to?",
+            when(answers) {
+                return answers.addEngineerOrInternOne === 'Intern'
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'What is the name of this engineer?',
+            when(answers) {
+                return answers.addEngineerOrInternOne === 'Engineer'
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerID',
+            message: "What is this engineer's employee ID?",
+            when(answers) {
+                return answers.addEngineerOrInternOne === 'Engineer'
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is this engineer's email address?",
+            when(answers) {
+                return answers.addEngineerOrInternOne === 'Engineer'
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerGitHub',
+            message: "What is this engineer's GitHub username?",
+            when(answers) {
+                return answers.addEngineerOrInternOne === 'Engineer'
+            }
         },
         {
             type: 'list',
@@ -135,10 +165,10 @@ inquirer
 
         };
 
-        const readmePageContent = generateREADME(answers);
+        const htmlPageContent = generateHTML(answers);
 
-        fs.writeFile('README2.md', readmePageContent, (err) =>
-            err ? console.log(err) : console.log('Successfully created README.md!')
+        fs.writeFile('index.html', htmlPageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created index.html!')
         );
     });
 
